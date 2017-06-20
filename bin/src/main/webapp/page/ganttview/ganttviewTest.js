e13e104496cdf1f68647d12ceaa5fd010183bdf5
lazy1993 <2280042554@qq.com>
2017-06-20 20:20:28 +0800

    Merge branch 'master' of https://github.com/lazy1993/ry

---
+++ bin/src/main/webapp/page/ganttview/ganttviewTest.js
@@ -0,0 +1,24 @@
+var data=
+	[
+		{"productName":"PD35筒身","orderTaking":"2016-03-03","begin":"2016-03-05","complete":"2016-04-01","delivery":"2016-04-03"},
+		{"productName":"PD35筒头","orderTaking":"2016-03-03","begin":"2016-03-05","complete":"2016-04-01","delivery":"2016-04-03"},
+		{"productName":"PD35灯座","orderTaking":"2016-03-03","begin":"2016-03-05","complete":"2016-04-11","delivery":"2016-04-13"},
+		{"productName":"PD35筒身","orderTaking":"2016-03-03","begin":"2016-03-05","complete":"2016-04-01","delivery":"2016-04-03"},
+		{"productName":"PD35筒身","orderTaking":"2016-03-03","begin":"2016-03-05","complete":"2016-04-03","delivery":"2016-04-05"},
+		{"productName":"PD35筒身","orderTaking":"2016-03-03","begin":"2016-03-05","complete":"2016-04-03","delivery":"2016-04-05"},
+		{"productName":"PD35筒身","orderTaking":"2016-03-03","begin":"2016-03-05","complete":"2016-04-03","delivery":"2016-04-05"},
+		{"productName":"PD35筒身","orderTaking":"2016-03-03","begin":"2016-03-05","complete":"2016-04-03","delivery":"2016-04-05"},
+		{"productName":"PD35筒身","orderTaking":"2016-03-03","begin":"2016-03-09","complete":"2016-04-05","delivery":"2016-04-07"},
+		{"productName":"PD35筒身","orderTaking":"2016-03-03","begin":"2016-03-09","complete":"2016-04-05","delivery":"2016-04-07"},
+		{"productName":"PD35筒身","orderTaking":"2016-03-03","begin":"2016-03-09","complete":"2016-03-25","delivery":"2016-03-27"},
+		{"productName":"PD35筒身","orderTaking":"2016-03-03","begin":"2016-03-11","complete":"2016-04-05","delivery":"2016-04-07"},
+		{"productName":"PD35筒身","orderTaking":"2016-03-03","begin":"2016-03-11","complete":"2016-04-07","delivery":"2016-04-10"},
+		{"productName":"PD35筒身","orderTaking":"2016-03-03","begin":"2016-03-13","complete":"2016-04-07","delivery":"2016-04-10"},
+		{"productName":"PD35筒身","orderTaking":"2016-03-03","begin":"2016-03-13","complete":"2016-04-07","delivery":"2016-04-10"},
+		{"productName":"PD35筒身","orderTaking":"2016-03-03","begin":"2016-03-13","complete":"2016-04-07","delivery":"2016-04-10"},
+		{"productName":"PD35筒身","orderTaking":"2016-03-03","begin":"2016-03-13","complete":"2016-04-09","delivery":"2016-04-12"},
+		{"productName":"PD35筒身","orderTaking":"2016-03-03","begin":"2016-03-20","complete":"2016-04-09","delivery":"2016-04-12"},
+		{"productName":"PD35筒身","orderTaking":"2016-03-03","begin":"2016-03-20","complete":"2016-04-01","delivery":"2016-04-03"},
+		{"productName":"PD35筒身","orderTaking":"2016-03-03","begin":"2016-03-20","complete":"2016-04-09","delivery":"2016-06-22"}
+
+	];
