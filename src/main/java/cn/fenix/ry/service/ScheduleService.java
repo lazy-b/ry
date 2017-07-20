cecfaa55c24a4989be246d693c35c461c263662c
wenyelist <695629876@qq.com>
2017-07-20 10:34:05 +0800

    测试提交
    测试

--- src/main/java/cn/fenix/ry/service/ScheduleService.java
+++ src/main/java/cn/fenix/ry/service/ScheduleService.java
@@ -10,11 +10,11 @@ import cn.fenix.ry.util.CustomResult;
 
 public interface ScheduleService {
 	//根据订单号和产品名称查出进度表
-	List<Map<String,Object>> listSchedule(String orderNo,String productName) throws Exception;
-	//增加进度信息
-	CustomResult insert(Schedule schedule) throws Exception;
-	//部分修改
-	CustomResult updateSchedule(Schedule schedule) throws NotOrderInformationFound;
-	//批量删除订单
-	CustomResult deleteBatchs(String[] ids) throws Exception;
+		List<Map<String,Object>> listSchedule(String orderNo,String productName) throws Exception;
+		//增加进度信息
+		CustomResult insert(Schedule schedule) throws Exception;
+		//部分修改
+		CustomResult updateSchedule(Schedule schedule) throws NotOrderInformationFound;
+		//批量删除订单
+		CustomResult deleteBatchs(String[] ids) throws Exception;
 }
